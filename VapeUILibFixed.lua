dc6c303052b3137b35bad996f6ef531b063f92eb
imnotokay34ie9r8ek <109005724+imnotokay34ie9r8ek@users.noreply.github.com>
2022-11-11 15:12:38 +0800

    Add files via upload

--- VapeUILibFixed.lua
+++ VapeUILibFixed.lua
@@ -141,31 +141,29 @@ function lib:Window(text, preset, closebind)
     MakeDraggable(DragFrame, Main)
 
     local uitoggled = false
-    task.spawn(function()
-        game.RunService.Stepped:connect(function()
-            UserInputService.InputBegan:Connect(
-                function(io, p)
-                    if io.KeyCode == CloseBind then
-                        if uitoggled == false then
-                            Main:TweenSize(UDim2.new(0, 0, 0, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .6, true)
-                            uitoggled = true
-                            wait(.5)
-                            knixhub.Enabled = false
-                        else
-                            Main:TweenSize(
-                                UDim2.new(0, 560, 0, 319),
-                                Enum.EasingDirection.Out,
-                                Enum.EasingStyle.Quart,
-                                .6,
-                                true
-                            )
-                            knixhub.Enabled = true
-                            uitoggled = false
-                        end
+    game.RunService.Stepped:connect(function()
+        UserInputService.InputBegan:Connect(
+            function(io, p)
+                if io.KeyCode == CloseBind then
+                    if uitoggled == false then
+                        Main:TweenSize(UDim2.new(0, 0, 0, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .6, true)
+                        uitoggled = true
+                        wait(.5)
+                        knixhub.Enabled = false
+                    else
+                        Main:TweenSize(
+                            UDim2.new(0, 560, 0, 319),
+                            Enum.EasingDirection.Out,
+                            Enum.EasingStyle.Quart,
+                            .6,
+                            true
+                        )
+                        knixhub.Enabled = true
+                        uitoggled = false
                     end
                 end
-            )
-        end)
+            end
+        )
     end)
 
     TabFolder.Name = "TabFolder"
